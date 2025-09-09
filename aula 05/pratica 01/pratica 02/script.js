295ac65fe44f70e8576eaf6a22aefbb17806afa5
Luana <luana.guilherme@unidavi.edu.br>
2025-09-08 21:16:49 -0300

    Notas dos Alunos

---
+++ aula 05/pratica 01/pratica 02/script.js
@@ -0,0 +1,39 @@
+function linha() {
+  let tabela = document.getElementById("tabelaNotas");
+  let qtdLinhas = tabela.rows.length;
+  let qtdColunas = tabela.rows[0].cells.length;
+
+  let novaLinha = tabela.insertRow(qtdLinhas);
+
+  // primeira célula
+  novaLinha.insertCell(0).innerHTML = "Médias";
+
+  // calcular médias de cada coluna
+  for (let c = 1; c < qtdColunas; c++) {
+    let soma = 0;
+    for (let l = 1; l < qtdLinhas; l++) {
+      soma += parseFloat(tabela.rows[l].cells[c].innerHTML);
+    }
+    let media = soma / (qtdLinhas - 1);
+    novaLinha.insertCell(c).innerHTML = media.toFixed(1);
+  }
+}
+
+function coluna() {
+  let tabela = document.getElementById("tabelaNotas");
+  let qtdLinhas = tabela.rows.length;
+  let qtdColunas = tabela.rows[0].cells.length;
+
+  // cabeçalho
+  tabela.rows[0].insertCell(qtdColunas).innerHTML = "Média";
+
+  // calcular médias por aluno
+  for (let l = 1; l < qtdLinhas; l++) {
+    let soma = 0;
+    for (let c = 1; c < qtdColunas; c++) {
+      soma += parseFloat(tabela.rows[l].cells[c].innerHTML);
+    }
+    let media = soma / (qtdColunas - 1);
+    tabela.rows[l].insertCell(qtdColunas).innerHTML = media.toFixed(1);
+  }
+}
